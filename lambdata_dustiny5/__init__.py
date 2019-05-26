543bcda3c3c1c67df13dbd24c49d04c4848a1b0e
dustiny5 <dustiny5@users.noreply.github.com>
2019-05-26 14:36:06 -0700

    Adding inital POC package/module

---
+++ lambdata_dustiny5/__init__.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python
+'''
+lambdata - A collection of Data Science helper functions
+'''
+
+import pandas as pd
+import numpy as np
+from . import example_module
+
+Y = example_module.increment(example_module.x)
+
+TEST = pd.DataFrame(np.ones(10))
