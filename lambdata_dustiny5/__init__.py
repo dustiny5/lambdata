156c4782ec1a9f06a26ec08b52b490cf65e00d9d
dustiny5 <dustiny5@users.noreply.github.com>
2019-05-28 14:02:20 -0700

    Added concat module and set feature, df to None

--- lambdata_dustiny5/__init__.py
+++ lambdata_dustiny5/__init__.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 from . import example_module
 #from . import train_test_split as tts
-#from . import concat
+from . import concat
 
 TEST = pd.DataFrame(np.ones(10))
 
@@ -18,7 +18,7 @@ Y = example_module.increment(example_module.x)
 #feature = [1,2,3,4]
 #d = {'col1': [1,2,3,4], 'col2': [1,2,3,4]}
 
-#NEW_DF = concat.concat_feature(concat.feature,concat.df)
+NEW_DF = concat.concat_feature(feature=None,df=None)
 
 # train_test_split and train_val_split
 #X, y = np.arange(10).reshape((5, 2)), range(5)
