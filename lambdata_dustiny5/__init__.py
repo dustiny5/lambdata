ad0577399c8a21f539e2fcbbb2f53a5ee9bff396
dustiny5 <dustiny5@users.noreply.github.com>
2019-05-27 20:14:23 -0700

    Revise train_test and train_val

--- lambdata_dustiny5/__init__.py
+++ lambdata_dustiny5/__init__.py
@@ -18,11 +18,11 @@ Y = example_module.increment(example_module.x)
 # concat
 feature = [1,2,3,4]
 d = {'col1': [1,2,3,4], 'col2': [1,2,3,4]}
-df = pd.DataFrame(d )
+df = pd.DataFrame(d)
 NEW_DF = concat.concat_feature(feature,df)
 
-# train_test_split
+# train_test_split and train_val_split
 X, y = np.arange(10).reshape((5, 2)), range(5)
-X_train, X_test, y_train, y_test = train_test_split.train_test(X, y)
+TRAIN_TEST = train_test_split.train_test(X, y)
 
-X_train, X_val, y_train, y_val = train_test_split.train_val(X_train, y_train)
+TRAIN_VAL = train_test_split.train_val(TRAIN_TEST[0], TRAIN_TEST[2])
