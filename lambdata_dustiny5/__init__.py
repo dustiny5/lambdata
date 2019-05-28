5af76e9ccd5896caa908b6e3364cf7e948413aed
dustiny5 <dustiny5@users.noreply.github.com>
2019-05-28 13:56:25 -0700

    Delete concat, tts, update version

--- lambdata_dustiny5/__init__.py
+++ lambdata_dustiny5/__init__.py
@@ -6,8 +6,8 @@ lambdata - A collection of Data Science helper functions
 import pandas as pd
 import numpy as np
 from . import example_module
-from . import train_test_split as tts
-from . import concat
+#from . import train_test_split as tts
+#from . import concat
 
 TEST = pd.DataFrame(np.ones(10))
 
@@ -18,13 +18,13 @@ Y = example_module.increment(example_module.x)
 #feature = [1,2,3,4]
 #d = {'col1': [1,2,3,4], 'col2': [1,2,3,4]}
 
-NEW_DF = concat.concat_feature(concat.feature,concat.df)
+#NEW_DF = concat.concat_feature(concat.feature,concat.df)
 
 # train_test_split and train_val_split
 #X, y = np.arange(10).reshape((5, 2)), range(5)
 
 # Module returns X_train, X_test, y_train, y_test
-TRAIN_TEST = tts.train_test(tts.X, tts.y)
+#TRAIN_TEST = tts.train_test(tts.X, tts.y)
 
 # Module returns X_train, X-val, y_train, y_val
-TRAIN_VAL = tts.train_val(TRAIN_TEST[0], TRAIN_TEST[2])
+#TRAIN_VAL = tts.train_val(TRAIN_TEST[0], TRAIN_TEST[2])
