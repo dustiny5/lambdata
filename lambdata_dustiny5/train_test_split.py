ef8a8154c0230eec43bdb847f9fc79524d468613
dustiny5 <dustiny5@users.noreply.github.com>
2019-05-30 17:02:36 -0700

    Added test.py to test for concat module

--- lambdata_dustiny5/train_test_split.py
+++ lambdata_dustiny5/train_test_split.py
@@ -57,3 +57,4 @@ class TrainTestSplit:
 #     '''
 #     X_train, X_val, y_train, y_val = train_test_split(feature, target, test_size=0.2, random_state=42)
 #     return X_train, X_val, y_train, y_val
+
